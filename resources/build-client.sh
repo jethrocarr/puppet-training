325b351ce70bd10b3124e5c4ef64d699cddfb15d
Jethro Carr <jethro.carr@fairfaxmedia.co.nz>
2016-10-11 17:32:43 +1300

    Added userdata hooks

--- resources/build-client.sh
+++ resources/build-client.sh
@@ -2,8 +2,13 @@
 #
 # Build a client server capable of connecting to the Puppet master
 #
+(
+exec 1> >(logger -s -t user-data) 2>&1
 
 # Update operating system
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
+
+
+)
--- resources/build-master.sh
+++ resources/build-master.sh
@@ -4,3 +4,14 @@
 # is simplistic in nature to provide the functionality needed for training
 # purposes only and is not recommended for real-world production Puppet master
 # builds.
+(
+exec 1> >(logger -s -t user-data) 2>&1
+
+
+# Update operating system
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y upgrade
+
+
+)
